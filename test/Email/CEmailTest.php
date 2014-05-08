d1cb18bd129c83c0ae640f4e4aac2915292ae309
joxiba <j.sehlstedt@me.com>
2014-05-08 13:21:13 +0100

    Test modified.

--- test/Email/CEmailTest.php
+++ test/Email/CEmailTest.php
@@ -19,9 +19,14 @@ class CEmailTest extends \PHPUnit_Framework_TestCase
     {
         $email = new \Jox\Email\CEmail();
 
+        // Test a valid email address.
         $res = $email->checkAddress('whatever@example.com');
         $exp = 'whatever@example.com';
         $this->assertEquals($res, $exp, "Email address validation failed.");
+
+        // Test for FALSE on an invalid email address.
+        $res = $email->checkAddress('whatever@example');
+        $this->assertFalse($res, "Invalid email address was not returnded as FALSE.");
     }
 
 
@@ -34,5 +39,11 @@ class CEmailTest extends \PHPUnit_Framework_TestCase
      */
     public function testSendEmail() 
     {
+        $email = new \Jox\Email\CEmail();
+
+        // Test with invalid email addresses.
+        $res = $email->sendEmail();
+        $exp = '! Email address not valid !';
+        $this->assertEquals($res, $exp, "Invalid email address didn't stop the sendEmail.");
     }
 }
